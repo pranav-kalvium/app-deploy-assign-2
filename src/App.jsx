31e21f102cca243b1c9f845af2b7bf7d2d19b2e6
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-02-12 09:57:28 +0530

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}>
+      <h1>My Name:Pranav Prakash</h1>
+      <p>Email: pranavprakash@gmail.com</p>
+      <p>Phone: +91592758392</p>
+    </div>
+  );
+}
+
+export default App;
